30125aeda546d6f2cbd4e7ce62900f0f5e8c407e
Asraf Ali <mdashraf9135@gmail.com>
2025-06-10 12:52:14 +0530

    add files

---
+++ src/components/First.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const First = () =>{
+    return(
+        <>
+            <h1>Hello External Components</h1>
+        </>
+    )
+}
+
+export default First    // you can import or call in any file
